5bb6df45ae508ef4a1d48f8427568997c67f5a96
serega.6600 <serega.6600@yandex.ru>
2023-02-26 21:39:44 +0300

    home work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите желаемую строку для обработки: ");
+
+string originalString = Console.ReadLine();
+string[] enteredArray = originalString.Split(' ', StringSplitOptions.RemoveEmptyEntries); // Формирование массива строк из введённого предложения. Разделение по пробелу 
+    
+    Console.WriteLine("Исходный массив: ");
+
+    for (int i = 0; i < enteredArray.Length; i++)
+    Console.Write(enteredArray[i] + " ");  // Вывод на экран введённой строки в виде массива строк и без разрывов 
+    Console.WriteLine();
+
+Console.WriteLine("Отсортированный массив: ");
+
+var resultString = new string[enteredArray.Length];  // ЗАдаём новый массив, куда будут записываться отсортированные значения
+var size = 0;
+foreach (var value in enteredArray)
+{
+if (value.Length <= 3)  // сортировка будет происходить по трём символам, включая знаки препинания и спецсимвола, если в параметре split не было указано иное
+   {
+    resultString[size] = value;
+    Console.Write(resultString[size] + ' ');  //вывод нового массива из строк 3 и менее символов
+    size++;
+   }
+}
